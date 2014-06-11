c83ed35103e82d4531aa5ab0d66509348c02ae44
ExpatPaul <paulpritchard68@gmail.com>
2014-06-11 22:48:37 +0200

    Sending SMTP now working but not available

--- squirt/smtplayer.py
+++ squirt/smtplayer.py
@@ -28,7 +28,7 @@ def smtp_send(script):
     server = smtplib.SMTP(script.get('server'), script.get('port'))
     server.starttls()
     server.ehlo_or_helo_if_needed()
-    server.login(script.get('user'), scrip.get('password'))
+    server.login(script.get('user'), script.get('password'))
     
     smtp_from = script.get('mailfrom')
     smtp_to = [script.get('mailto')]
